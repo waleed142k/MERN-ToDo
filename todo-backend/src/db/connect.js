160b08ef7767e3a3eb26859b11cedab25912a112
waleed afzaal <waleedatwork14@gmail.com>
2023-04-17 18:10:53 +0500

    first commit

---
+++ todo-backend/src/db/connect.js
@@ -0,0 +1,27 @@
+// external modules import
+const mongoose = require("mongoose");
+
+const connectionString = "mongodb+srv://todo-db.cksosd5.mongodb.net/TODO-APP";
+// const connectionString = "mongodb+srv://waleedatwork14:waleedatwork14@todo-db.cksosd5.mongodb.net/TODO-APP";
+
+const connectDatabase = async () => {
+  try {
+    await mongoose
+      .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true, // for mongoose 6.x
+        // useFindAndModify: false, // for mongoose 6.x
+      })
+      .then(() => {
+        console.log("Connected to MongoDB database successfully.");
+      })
+      .catch((error) => {
+        console.log("Error connecting to MongoDB: ", error.message);
+      });
+  } catch (error) {
+    console.log("Database connection error: ", error.message);
+  }
+};
+
+module.exports = connectDatabase;
